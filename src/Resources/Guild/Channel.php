5afc15e6018db9b4f00374f165e7ccab95b2f0c9
luceos <daniel+github@klabbers.email>
2017-11-16 16:03:00 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Resources/Guild/Channel.php
+++ src/Resources/Guild/Channel.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the Discodian bot toolkit.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://discodian.com
+ * @see https://github.com/discodian
+ */
+
 namespace Discodian\Core\Resources\Guild;
 
 use Discodian\Core\Resources\Part;
@@ -18,5 +30,4 @@ use Discodian\Core\Resources\Part;
  */
 class Channel extends Part
 {
-
 }
