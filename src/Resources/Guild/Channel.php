0c117f2679586e696a834b78a124dd94e72d09eb
Daniel Klabbers <daniel@productip.com>
2017-11-16 17:02:45 +0100

    add guild channel

---
+++ src/Resources/Guild/Channel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Discodian\Core\Resources\Guild;
+
+use Discodian\Core\Resources\Part;
+
+/**
+ * @property int $id
+ * @property int $bitrate
+ * @property int $guild_id
+ * @property bool $is_private
+ * @property int $last_message_id
+ * @property string $name
+ * @property int $position
+ * @property string $topic
+ * @property string $type
+ * @property int $user_limit
+ */
+class Channel extends Part
+{
+
+}
