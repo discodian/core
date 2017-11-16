5360a112bf79e8a3c8881595ea8f01920fbc2cb7
luceos <daniel+github@klabbers.email>
2017-11-16 15:55:08 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Resources/Guild/Role.php
+++ src/Resources/Guild/Role.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the Discodian bot toolkit.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://discodian.com
+ * @see https://github.com/discodian
+ */
+
 namespace Discodian\Core\Resources\Guild;
 
 use Discodian\Core\Resources\Part;
@@ -16,5 +28,4 @@ use Discodian\Core\Resources\Part;
  */
 class Role extends Part
 {
-
 }
