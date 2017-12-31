46098526cec23bf5a90ad591e1680340d52a333e
Daniël Klabbers <daniel@klabbers.email>
2017-12-31 15:18:05 +0100

    fix role update

--- src/Socket/Events/GuildRoleUpdate.php
+++ src/Socket/Events/GuildRoleUpdate.php
@@ -15,7 +15,7 @@
 namespace Discodian\Core\Socket\Events;
 
 use Discodian\Core\Socket\Event;
-use Discord\Parts\Guild\Role;
+use Discodian\Parts\Guild\Role;
 use React\Promise\Deferred;
 
 class GuildRoleUpdate extends Event
