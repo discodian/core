71f9e86b6ea62bce6fce1b8b2d1a3607bf8905be
Daniel Klabbers <daniel@productip.com>
2018-01-10 17:07:09 +0100

    added scheduled tasks can now return a response

--- src/Listeners/SchedulingHandler.php
+++ src/Listeners/SchedulingHandler.php
@@ -3,10 +3,15 @@
 namespace Discodian\Core\Listeners;
 
 use Discodian\Core\Events\Ws\Heartbeat;
+use Discodian\Core\Response\Factory;
+use Discodian\Extend\Responses\Response;
+use Illuminate\Console\Scheduling\CallbackEvent;
 use Illuminate\Console\Scheduling\Event;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Arr;
+use React\Promise\Deferred;
 
 class SchedulingHandler
 {
@@ -18,11 +23,16 @@ class SchedulingHandler
      * @var Application
      */
     private $app;
+    /**
+     * @var Factory
+     */
+    private $response;
 
-    public function __construct(Schedule $schedule, Application $app)
+    public function __construct(Schedule $schedule, Application $app, Factory $response)
     {
         $this->schedule = $schedule;
         $this->app = $app;
+        $this->response = $response;
     }
 
     public function subscribe(Dispatcher $events)
@@ -32,14 +42,34 @@ class SchedulingHandler
 
     public function run(Heartbeat $event)
     {
+        $defer = new Deferred();
+
+        $due = $this->schedule->dueEvents($this->app);
+        $completed = null;
         /** @var Event $event */
-        foreach ($this->schedule->dueEvents($this->app) as $event) {
+        foreach ($due as $event) {
             if (! $event->filtersPass($this->app)) {
                 continue;
             }
 
+            if (!($event instanceof CallbackEvent)) {
+                throw new \InvalidArgumentException("Only callback events allowed.");
+            }
+
             logs("running event: ". $event->getSummaryForDisplay());
-            $event->run($this->app);
+            $response = $event->run($this->app);
+
+            if (is_array($response)) {
+                $response = Arr::first($response, function ($item) {
+                    return $item instanceof Response;
+                });
+            }
+
+            if ($response instanceof Response) {
+                $this->response->respond($response);
+            }
         }
+
+        $defer->resolve([$due, $completed]);
     }
 }
