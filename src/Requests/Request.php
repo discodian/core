0a1aae8cc8e5c64b9c13728e8c977930089c2332
Daniel Klabbers <daniel@productip.com>
2018-01-18 16:36:13 +0100

    readying up to take rate limiting into account

--- src/Requests/Request.php
+++ src/Requests/Request.php
@@ -14,10 +14,13 @@
 
 namespace Discodian\Core\Requests;
 
+use Carbon\Carbon;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Promise\Promise;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Arr;
+use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
 use Symfony\Component\HttpFoundation\HeaderBag;
 
 /**
@@ -27,9 +30,12 @@ use Symfony\Component\HttpFoundation\HeaderBag;
  */
 abstract class Request
 {
-    protected $rate_limit;
-    protected $rate_reset;
-    protected $rate_remaining;
+    protected $rateLimited = false;
+    protected $queued = [];
+    protected $rate_limit = [];
+    protected $rate_reset = [];
+    protected $rate_remaining = [];
+    protected $retry_after;
 
     /**
      * Path to send request to.
@@ -69,45 +75,118 @@ abstract class Request
      */
     public function request()
     {
-        return static::getHttp()
-            ->requestAsync($this->method, $this->getPath(), $this->buildParams())
-            ->then(function (Response $response) {
-                $this->processRateLimits(new HeaderBag($response->getHeaders()));
+        $defer = new Deferred();
 
-                $output = null;
+        $path = $this->getPath();
 
-                if ($response->getHeaderLine('content-type') === 'application/json') {
-                    $output = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+        $request = function () use (&$request, $path, $defer) {
+            $promise = static::getHttp()->requestAsync($this->method, $path, $this->buildParams());
 
-                    if (Arr::get($response, 'state') === 'fulfilled') {
-                        $output = Arr::get($response, 'value', []);
+            $promise->then(function (Response $response) use (&$request, $path, $defer) {
+                    $this->processRateLimits($path, new HeaderBag($response->getHeaders()));
+                    $this->preventRateLimiting($path, $response, $request);
+                    $this->handleEndpointFailures($response, $defer, $request);
+
+                    if ($response->getHeaderLine('content-type') === 'application/json') {
+                        $output = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+
+                        if (Arr::get($response, 'state') === 'fulfilled') {
+                            $output = Arr::get($response, 'value', []);
+                        }
+                    } else {
+                        $output = $response;
                     }
-                } else {
-                    $output = $response;
-                }
 
-                return $output;
-            }, function (\Exception $e) {
-                logs("Request failed {$e->getMessage()}", $e->getTrace());
-            });
+                    $defer->resolve($output);
+                }, function (\Exception $e) use ($defer) {
+                    logs("Request failed {$e->getMessage()}", $e->getTrace());
+                    $defer->reject($e);
+                });
+        };
+
+        if ($this->rateLimited) {
+            $defer = new Deferred();
+            $defer->promise()->then($request());
+            $this->queued[] = $defer;
+        } else {
+            $request();
+        }
+
+        return $defer->promise();
+    }
+
+    protected function handleEndpointFailures(Response $response, Deferred $defer, $request)
+    {
+        $code = $response->getStatusCode();
+        if (in_array($code, [502, 525])) {
+            static::getLoop()->addTimer(0.25, $request);
+        }
+
+        if ($code < 200 || $code > 226) {
+            $defer->reject($response);
+        }
     }
 
     /**
+     * @param string $path
+     * @param Response $response
+     * @param $request
+     * @return bool
+     */
+    protected function preventRateLimiting(string $path, Response $response, $request): bool
+    {
+        $remaining = Arr::get($this->rate_remaining, $path);
+        $resetsAt = Arr::get($this->rate_reset, $path);
+
+        if ($response->getStatusCode() === 429 || (
+            $remaining !== null && $resetsAt !== null && $remaining === 0)) {
+            $this->rateLimited = true;
+
+            if (!$resetsAt && $this->retry_after) {
+                $waitFor = $this->retry_after/1000;
+            } else {
+                $waitFor = Carbon::now()->diffInSeconds(Carbon::createFromTimestamp($resetsAt));
+            }
+
+            // In case we hit the rate limit already, put the current request back on the stack.
+            if ($response->getStatusCode() === 429) {
+                $this->queued = $request;
+            }
+
+            static::getLoop()->addTimer($waitFor, function () {
+                foreach ($this->queued as $i => $item) {
+                    $item->resolve();
+                    unset($this->queued[$i]);
+                }
+
+                $this->rateLimited = false;
+            });
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $path
      * @param HeaderBag $headers
      */
-    protected function processRateLimits(HeaderBag $headers)
+    protected function processRateLimits(string $path, HeaderBag $headers)
     {
         if ($headers->has('x-ratelimit-limit')) {
-            $this->rate_limit = $headers->get('x-ratelimit-limit');
+            $this->rate_limit[$path] = $headers->get('x-ratelimit-limit');
         }
 
         if ($headers->has('x-ratelimit-reset')) {
-            $this->rate_reset = $headers->get('x-ratelimit-reset');
+            $this->rate_reset[$path] = $headers->get('x-ratelimit-reset');
         }
 
         if ($headers->has('x-ratelimit-remaining')) {
-            $this->rate_remaining = $headers->get('x-ratelimit-remaining');
+            $this->rate_remaining[$path] = $headers->get('x-ratelimit-remaining');
         }
+
+        $this->retry_after = $headers->get('retry-after');
     }
 
     /**
@@ -128,6 +207,11 @@ abstract class Request
         }
 
         return static::$http;
+    }
+
+    protected static function getLoop(): LoopInterface
+    {
+        return app(LoopInterface::class);
     }
 
     protected function buildParams(): array
